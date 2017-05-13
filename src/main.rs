f5cdd5bfd6d4c0ef7a217f436b20430863b2244c
tlrobrn <obrien.taylorm@gmail.com>
2017-05-12 22:25:13 -0500

    a more fun test for main

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use gol::*;
 fn main() {
     let points = [Point { x: 0, y: 0 },
                   Point { x: 1, y: 0 },
-                  Point { x: 0, y: 1 },
-                  Point { x: 1, y: 1 }];
+                  Point { x: 2, y: 0 }];
 
     let mut grid = Grid::new(points.iter());
 
-    for _ in 0..1000 {
+    for _ in 0..1001 {
         grid.tick();
     }
 
