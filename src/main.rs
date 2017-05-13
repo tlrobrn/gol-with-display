8d585961f87b75e88de4ec8f3e184927e947e015
tlrobrn <obrien.taylorm@gmail.com>
2017-05-12 22:22:40 -0500

    make sure it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate gol;
+use gol::*;
+
 fn main() {
-    println!("Hello, world!");
+    let points = [Point { x: 0, y: 0 },
+                  Point { x: 1, y: 0 },
+                  Point { x: 0, y: 1 },
+                  Point { x: 1, y: 1 }];
+
+    let mut grid = Grid::new(points.iter());
+
+    for _ in 0..1000 {
+        grid.tick();
+    }
+
+    println!("{:?}", grid);
 }
